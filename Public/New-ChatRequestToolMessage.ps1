c11bc3f5464108fd7c5b4855c9b0b08deaa24057
dfinke <finked@hotmail.com>
2024-05-27 17:49:56 -0400

    feat: Add New-ChatRequestToolMessage function for creating tool messages in chat requests

---
+++ Public/New-ChatRequestToolMessage.ps1
@@ -0,0 +1,40 @@
+<#
+.SYNOPSIS
+Creates a new chat request tool message.
+
+.DESCRIPTION
+The New-ChatRequestToolMessage function creates a new chat request tool message with the specified parameters.
+
+.PARAMETER toolCallId
+The ID of the tool call.
+
+.PARAMETER toolFunctionName
+The name of the tool function.
+
+.PARAMETER content
+The content of the tool message.
+
+.EXAMPLE
+New-ChatRequestToolMessage -toolCallId 123 -toolFunctionName "MyTool" -content "Hello, world!"
+
+This example creates a new chat request tool message with the tool call ID 123, tool function name "MyTool", and content "Hello, world!".
+
+#>
+function New-ChatRequestToolMessage {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        $toolCallId,
+        [Parameter(Mandatory)]
+        $toolFunctionName,
+        [Parameter(Mandatory)]
+        $content        
+    )
+  
+    @{
+        role         = 'tool'
+        tool_call_id = $toolCallId
+        name         = $toolFunctionName
+        content      = $content
+    }
+}
