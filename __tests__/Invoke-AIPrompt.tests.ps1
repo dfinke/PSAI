0e80d1abc7b1a2c69c30ef8a776bb77c4096ad1d
dfinke <finked@hotmail.com>
2024-07-25 10:14:09 -0400

    feat: Add tests for Invoke-AIPrompt function

---
+++ __tests__/Invoke-AIPrompt.tests.ps1
@@ -0,0 +1,19 @@
+Describe "Invoke-AIPrompt" -Tag Invoke-AIPrompt {
+    BeforeAll {
+        Import-Module "$PSScriptRoot/../PSAI.psd1" -Force
+    }
+
+    It "should have these parameters" {
+        $actual = Get-Command Invoke-AIPrompt -ErrorAction SilentlyContinue
+     
+        $actual | Should -Not -BeNullOrEmpty
+
+        $keyArray = $actual.Parameters.Keys -as [array]
+
+        $keyArray[0] | Should -BeExactly 'Prompt'
+        $keyArray[1] | Should -BeExactly 'Data'
+        $keyArray[2] | Should -BeExactly 'UsePowerShellPersona'
+
+        $actual.Parameters['UsePowerShellPersona'].SwitchParameter | Should -Be $true
+    }
+}
