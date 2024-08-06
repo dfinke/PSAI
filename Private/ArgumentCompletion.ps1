25ea16328ff05449e3a612af4c3f5342641d75a2
dfinke <finked@hotmail.com>
2024-08-06 17:40:13 -0400

    feat: Add 'gpt-4o-2024-08-06' model to ArgumentCompletion.ps1

--- Private/ArgumentCompletion.ps1
+++ Private/ArgumentCompletion.ps1
@@ -21,6 +21,7 @@ function ModelArgumentCompleter {
         "gpt-4-vision-preview"
         "gpt-4"
         "gpt-4o-2024-05-13"
+        "gpt-4o-2024-08-06"
         "gpt-4o-mini-2024-07-18"
         "gpt-4o-mini"
         "gpt-4o"
