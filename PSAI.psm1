aa5ae0196bca1b93c726f316485218d109a3f5d7
dfinke <finked@hotmail.com>
2024-10-25 07:11:15 -0400

    Add YouTubeTool and YouTubeAssistant modules to PSAI.psm1

--- PSAI.psm1
+++ PSAI.psm1
@@ -7,6 +7,10 @@
 Import-Module $PSScriptRoot/Public/Tools/CalculatorTool.psm1
 Import-Module $PSScriptRoot/Public/Tools/TavilyTool.psm1
 Import-Module $PSScriptRoot/Public/Tools/StockTickerTool.psm1
+Import-Module $PSScriptRoot/Public/Tools/YouTubeTool.psm1
+
+# Agent Assistants
+Import-Module $PSScriptRoot/Public/Tools/YouTubeAssistant.psm1
 
 # Public Functions
 . $PSScriptRoot/Public/New-Agent.ps1
