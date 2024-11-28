a70bf15d834107682ac9d6403b2fdbf9cbd059cf
Axel Bøg Andersen <axel.boeg.andersen@atea.dk>
2024-11-28 09:46:46 +0100

    Test both env and get-secret

--- __tests__/IntegrationTests/OpenAI.tests.ps1
+++ __tests__/IntegrationTests/OpenAI.tests.ps1
@@ -1,10 +1,11 @@
 BeforeDiscovery {
     $info = Get-SecretInfo -Name OpenAI
+    $env:OpenAIKey = Get-Secret OpenAI -asPlainText # replace with clear text password if Get-Secret is not setup on your system
 }
 
-Describe "Test chat endpoints" -Skip:($null -eq $info) {
+Describe "Test chat endpoints with Secret" -Skip:($null -eq $info) {
 
-    BeforeAll {
+    BeforeEach {
         Import-Module "$(Split-Path $(Split-Path $PSScriptRoot))/PSAI.psd1" -Force
         $params = @{
                 Provider = 'OpenAI'
@@ -13,8 +14,26 @@ Describe "Test chat endpoints" -Skip:($null -eq $info) {
         Import-AIProvider @params
     }
 
-    Context "Invoke-OAIChatCompletion" {
+    Context "Invoke-OAIChatCompletion with Secret" {
         It "Invoke-OAIChatCompletion generates an answer" -Skip:($null -eq $info) {
+            $Prompt = "What is the capitol of France"
+            $Message = New-OAIChatMessage -Role user -Content $Prompt
+            $Answer = Invoke-OAIChatCompletion -Message $Message
+            $Answer | Should -Not -BeNullOrEmpty
+            $Answer | Should -BeOfType [String]
+            $Answer | Should -Match "Paris"
+        }
+    }
+}
+
+Describe "Test chat endpoints with Environment Variable" -Skip:($null -eq $env:OpenAIKey) {
+
+    BeforeEach {
+        Import-Module "$(Split-Path $(Split-Path $PSScriptRoot))/PSAI.psd1" -Force
+    }
+
+    Context "Invoke-OAIChatCompletion with Environment Variable" {
+        It "Invoke-OAIChatCompletion generates an answer" -Skip:($null -eq $envSecret) {
             $Prompt = "What is the capitol of France"
             $Message = New-OAIChatMessage -Role user -Content $Prompt
             $Answer = Invoke-OAIChatCompletion -Message $Message
