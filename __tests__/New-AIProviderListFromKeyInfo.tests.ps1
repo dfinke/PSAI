5d0e2813b3b04f82ec0ecb355edb3aa012159453
Axel Bøg Andersen <axel.boeg.andersen@atea.dk>
2024-12-13 16:58:07 +0100

    Maybe this will make Mac shut up

--- __tests__/New-AIProviderListFromKeyInfo.tests.ps1
+++ __tests__/New-AIProviderListFromKeyInfo.tests.ps1
@@ -1,10 +1,17 @@
 Describe "Test New-AIProviderListFromKeyInfo" {
     BeforeAll {
         Import-Module "$PSScriptRoot/../PSAI.psd1" -Force
-   }
+        $backup = Get-AIKeyInfo -AsHashTable
+        $path = Join-Path $(Split-Path $PSScriptRoot) 'AIKeyInfo.json'
+        Remove-Item $path -ErrorAction SilentlyContinue
+    }
 
     It "Create a new ProviderList from environment variables" {
+        $env:OpenAIKey = 'OpenAISuperKeyEnv'
         New-AIProviderListFromKeyInfo
         Get-AIProvider -All | Should -Not -BeNullOrEmpty
     }
+    AfterAll {
+        $backup | ConvertTo-Json | Out-File $path
+     }
 }
