bb1ad05c393fa560660d131b11909f2a956bb0c6
Axel Bøg Andersen <axel.boeg.andersen@atea.dk>
2024-12-27 08:28:01 +0100

    Make sure OpenAI is first

--- __tests__/New-AIProviderListFromKeyInfo.tests.ps1
+++ __tests__/New-AIProviderListFromKeyInfo.tests.ps1
@@ -1,17 +1,44 @@
+
+
 Describe "Test New-AIProviderListFromKeyInfo" {
     BeforeAll {
         Import-Module "$PSScriptRoot/../PSAI.psd1" -Force
         $backup = Get-AIKeyInfo -AsHashTable
         $path = Join-Path $(Split-Path $PSScriptRoot) 'AIKeyInfo.json'
         Remove-Item $path -ErrorAction SilentlyContinue
+        $env:OpenAIKey = $null
+    
     }
 
+    BeforeEach {
+        $path = Join-Path $(Split-Path $PSScriptRoot) 'AIKeyInfo.json'
+        Remove-Item $path -ErrorAction SilentlyContinue
+        $env:OpenAIKey = $null
+        Clear-AIProviderList
+    }
+    
     It "Create a new ProviderList from environment variables" {
         $env:OpenAIKey = 'OpenAISuperKeyEnv'
         New-AIProviderListFromKeyInfo
-        Get-AIProvider -All | Should -Not -BeNullOrEmpty
+        $AllProviders = Get-AIProvider -All
+        $AllProviders | Should -Not -BeNullOrEmpty
+        $AllProviders.Count | Should -Be 1
+        (Get-AIProvider).GetApiKey() | Should -Be 'OpenAISuperKeyEnv'
+    }
+
+    it "Create a new ProviderList with multiple providers keeps order" {
+        $env:OpenAIKey = 'OpenAISuperKeyEnv'
+        $env:xAIKey = 'xAISuperKeyEnv'
+        $env:GeminiKey = 'GeminiSuperKeyEnv'
+        $env:AnthropicKey = 'AnthropicSuperKeyEnv'
+
+        New-AIProviderListFromKeyInfo
+        $provider = Get-AIProvider
+        $provider | Should -Not -BeNullOrEmpty
+        $provider.Name |Should -Be 'OpenAI'
     }
     AfterAll {
         $backup | ConvertTo-Json | Out-File $path
      }
 }
+
